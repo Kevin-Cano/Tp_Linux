f704166ad324e9ae301c8e70a87930bc97d10f1a
cakevin <kevin.cano@ynov.com>
2024-03-05 16:30:11 +0100

    add lolcat in yt-v2 (lolcat in yt commit before)

--- TP 5/yt-v2.sh
+++ TP 5/yt-v2.sh
@@ -17,14 +17,14 @@ while true; do
             exit 1
         fi
 
-        video_name=$(youtube-dl --get-title "$1"
+        video_name=$(youtube-dl --get-title "$1")
         mkdir -p "/srv/yt/downloads/$video_name"
-        youtube-dl -o "/srv/yt/downloads/$video_name/$video_name.mp4" "$1"
-        youtube-dl --write-description -o "/srv/yt/downloads/$video_name/description" "$1"
-        echo "Video $1 was downloaded."
-        echo "File path : /srv/yt/downloads/$video_name/$video_name.mp4"
-        date=$(date +'%Y/%m/%d %H:%M:%S')
-        echo "[$date] Video $1 was downloaded. File path : /srv/yt/downloads/$video_name/$video_name.mp4" >> /var/log/yt/download.log
+        youtube-dl -o "/srv/yt/downloads/$video_name/$video_name.mp4" "$1" | lolcat
+        youtube-dl --write-description -o "/srv/yt/downloads/$video_name/description" "$1" | lolcat
+        echo "Video $1 was downloaded." | lolcat
+        echo "File path : /srv/yt/downloads/$video_name/$video_name.mp4" | lolcat
+        date=$(date +'%Y/%m/%d %H:%M:%S') | lolcat
+        echo "[$date] Video $1 was downloaded. File path : /srv/yt/downloads/$video_name/$video_name.mp4" >> /var/log/yt/download.log | lolcat
     fi
     sed -i '1d' $file
     done < "$file"
