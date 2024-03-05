0f06f7d2254f426caf8b48cb1cf3221c72fb64d2
cakevin <kevin.cano@ynov.com>
2024-03-05 16:25:55 +0100

    TP 5

--- TP 5/yt.sh
+++ TP 5/yt.sh
@@ -10,11 +10,11 @@ if [ ! -d "/var/log/yt/" ]; then
     exit 1
 fi
 
-video_name=$(youtube-dl --get-title "$1"
-mkdir -p "/srv/yt/downloads/$video_name"
-youtube-dl -o "/srv/yt/downloads/$video_name/$video_name.mp4" "$1"
-youtube-dl --write-description -o "/srv/yt/downloads/$video_name/description" "$1"
-echo "Video $1 was downloaded."
-echo "File path : /srv/yt/downloads/$video_name/$video_name.mp4"
-date=$(date +'%Y/%m/%d %H:%M:%S')
-echo "[$date] Video $1 was downloaded. File path : /srv/yt/downloads/$video_name/$video_name.mp4" >> /var/log/yt/download.log
+video_name=$(youtube-dl --get-title "$1") 
+mkdir -p "/srv/yt/downloads/$video_name" 
+youtube-dl -o "/srv/yt/downloads/$video_name/$video_name.mp4" "$1" | lolcat
+youtube-dl --write-description -o "/srv/yt/downloads/$video_name/description" "$1" | lolcat
+echo "Video $1 was downloaded." | lolcat
+echo "File path : /srv/yt/downloads/$video_name/$video_name.mp4" | lolcat
+date=$(date +'%Y/%m/%d %H:%M:%S') | lolcat
+echo "[$date] Video $1 was downloaded. File path : /srv/yt/downloads/$video_name/$video_name.mp4" >> /var/log/yt/download.log | lolcat
